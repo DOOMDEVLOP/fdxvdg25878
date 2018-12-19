bfaead2fd3ddaad1799fc9e39026727ab98b2839
DOOMDEVLOP <46000838+DOOMDEVLOP@users.noreply.github.com>
2018-12-19 16:35:21 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,64 @@
+const Discord = require ("discord.js");
+const bot = new Discord.Client();
+var prefix = ("!")
+const YTDL = require("ytdl-core");
+const {get} = require ("snekfetch");
+bot.login('NDcwOTc2NDgzMDAzOTkwMDE2.DvrSCw.xA-LS7tT0CQUUI6hr1uy1bcQsxY');
+
+
+bot.on("ready", function() {
+    console.log("Connecté");
+});
+
+bot.on("message", function(message) {
+    if(message.author.equals(bot.user)) return;
+    
+    if (!message.content.startsWith(prefix)) return;
+
+    var args = message.content.substring(prefix.length).split(" ");
+
+    switch (args[0].toLowerCase()) {
+        case "roll":
+            var roll = Math.floor(Math.random() * args[1]) +1;
+            if (!roll) return message.reply("Entre un numéro")
+            message.channel.send("Je choisis le numéro " + roll + " !");
+            break;
+        case "8ball":
+        let arfs = message.content.split(" ").slice(1);
+        let tte = args.join(" ")
+        if (!tte){
+            return message.reply("Merci de poser une question :8ball:")};
+
+            var replys = [
+                "Oui",
+                "Non",
+                "Je sais pas",
+                "Peut être"
+            ];
+            
+            let reponse = (replys[Math.floor(Math.random() * replys.length)])
+            var bembed = new Discord.RichEmbed()
+            .setDescription(":8ball: 8ball")
+            .addField("Question", tte)
+            .addField("Réponse", reponse)
+        message.channel.sendEmbed(bembed)    
+     
+}})
+
+bot.on("message", function(message) {
+    if(message.content.startsWith(prefix)) return;
+    switch (args[0].toLowerCase()) {
+        case "pussy":
+        try{
+            get('https://www.sex.com/gifs/pussy').then(res=> {
+                const embed = new Discord.RichEmbed()
+                .setDescription(`:pussy: pussy pic ${message.author.username}`)
+                .setImage(res.body.file)
+                .setColor("0xF000FF")
+                return message.channel.send({embed});
+            });
+        } catch(err) {
+            return message.channel.send(error.stack);
+        }
+            break;
+    }})
